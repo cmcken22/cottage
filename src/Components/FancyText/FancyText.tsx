ee0c31c3823842cc4640e9aebfca6919036c9151
Conner McKenna <cmckenna@ellisdon.com>
2025-03-20 20:58:53 -0400

    chore: updating terms text styles

--- src/Components/FancyText/FancyText.tsx
+++ src/Components/FancyText/FancyText.tsx
@@ -1,18 +1,10 @@
-import { useQuery } from "@hooks/index";
 import styles from "./FancyText.module.scss";
 import { FancyTextProps } from "./FancyText.types";
 import cx from "classnames";
 
 const FancyText = ({ children }: FancyTextProps) => {
-  const query = useQuery();
-  const blackMode = query.get("black") === "true";
-
   return (
-    <div
-      className={cx(styles.fancyText, {
-        [styles["fancyText--black"]]: blackMode,
-      })}
-    >
+    <div className={cx(styles.fancyText)}>
       <span className={styles.text}>{children}</span>
       <span className={styles.dropText}>{children}</span>
     </div>
