5f90016a3bb5d51f99197646812f0b78abeaf8bd
Conner McKenna <cmckenna@ellisdon.com>
2025-03-20 20:32:28 -0400

    chore: adding black version of card

--- src/Components/FancyText/FancyText.tsx
+++ src/Components/FancyText/FancyText.tsx
@@ -1,9 +1,18 @@
+import { useQuery } from "@hooks/index";
 import styles from "./FancyText.module.scss";
 import { FancyTextProps } from "./FancyText.types";
+import cx from "classnames";
 
 const FancyText = ({ children }: FancyTextProps) => {
+  const query = useQuery();
+  const blackMode = query.get("black") === "true";
+  console.log("blackMode:", blackMode);
   return (
-    <div className={styles.fancyText}>
+    <div
+      className={cx(styles.fancyText, {
+        [styles["fancyText--black"]]: blackMode,
+      })}
+    >
       <span className={styles.text}>{children}</span>
       <span className={styles.dropText}>{children}</span>
     </div>
